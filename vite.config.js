3f2fc4e09437cc7a18ef7d14291efb459f1d19ab
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-09-10 12:58:36 +0300

    value system section

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Set the development server port
+     // Set the development server port
     open: true, // Automatically open the app in the browser
   },
   build: {
