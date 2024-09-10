842e84f0349fbd46d7064ea595b334b0aa6e8e3f
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-09-10 12:11:27 +0300

    landing page

---
+++ src/components/HomeComponents.jsx/LandingPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import BillBoard from '../../assets/images/billboard.png'
+import HomeLeftShape from '../../components/HomeLeftShape'
+import Button from '../../components/Button'
+
+const LandingPage = () => {
+  return (
+    <div className='landing_page'>
+         <div className="billboard_image">
+            <img src={BillBoard} alt="An image of a billboard" />
+        </div>
+        <div className="color_overlay">
+            {/* gradient color overlay */}
+        </div>
+        <div className="texts_section">
+            <div className="top_text">
+                <div className="upper"> BRINGING YOUR BRAND</div>
+                <div className="lower">To life: <span className="yellow">outdoor</span></div>
+            </div>
+            <div className="bottom_text">
+                <div className="upper">-Indoor & outdoor branding -Digital Marketing</div>
+                <div className="lower">- Digital screen advertising</div>
+            </div>
+            <div className="btn"><Button content={"Talk to us"}/></div>
+        </div>
+        <div className="leftside_shape">
+            <HomeLeftShape />
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,10 @@
 import './styles/home.css'
-import BillBoard from '../assets/images/billboard.png'
-import HomeLeftShape from '../components/HomeLeftShape'
-import Button from '../components/Button'
+import LandingPage from '../components/HomeComponents.jsx/LandingPage'
 
 const Home = () => {
   return (
     <div className='home'>
-        <div className="billboard_image">
-            <img src={BillBoard} alt="An image of a billboard" />
-        </div>
-        <div className="color_overlay">
-            {/* gradient color overlay */}
-        </div>
-        <div className="texts_section">
-            <div className="top_text">
-                <div className="upper"> BRINGING YOUR BRAND</div>
-                <div className="lower">To life: <span className="yellow">outdoor</span></div>
-            </div>
-            <div className="bottom_text">
-                <div className="upper">-Indoor & outdoor branding -Digital Marketing</div>
-                <div className="lower">- Digital screen advertising</div>
-            </div>
-            <div className="btn"><Button content={"Talk to us"}/></div>
-        </div>
-        <div className="leftside_shape">
-            <HomeLeftShape />
-        </div>
+       <LandingPage />
     </div>
   )
 }
