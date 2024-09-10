fce9962e6eb3d91fe453d96983b63929dfabc7d9
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-09-10 12:36:14 +0300

    value system section

--- src/components/HomeComponents.jsx/Values.jsx
+++ src/components/HomeComponents.jsx/Values.jsx
@@ -1,9 +1,13 @@
+import '../styles/values.css'
 import Woman from '../../assets/images/woman.png'
 const Values = () => {
   return (
     <div className="values">
       <div className="woman_image">
-        {/* <img src={Woman} alt="A woman's image" /> */}
+        <img src={Woman} alt="A woman's image" />
+      </div>
+      <div className="right_section">
+        
       </div>
     </div>
   );
