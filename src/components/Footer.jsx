b2178a122e56515cfbdb637d6e484ff353f7cf54
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-09-19 18:27:36 +0300

    code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './styles/footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          <h3>Zamar Solutions Ltd</h3>
+          <p>Empowering Businesses with Digital Solutions.</p>
+          <p>&copy; {new Date().getFullYear()} Zamar Solutions Ltd. All rights reserved.</p>
+          <br />
+          <br />
+          <p>Call: 0720968693</p>
+          <p>Call: 0745686343</p>
+          <br />
+          <br />
+          <p>Email: info@zamarsolution.co.ke</p>
+        </div>
+
+        <div className="footer-right">
+          <iframe
+            src="https://www.example.com"
+            width="300"
+            height="200"
+            style={{ border: 'none' }}
+            title="Zamar Solutions Map"
+          ></iframe>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
